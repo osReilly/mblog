5f662f10ce54f11bfbaefbfe4690ec3fef960b30
osReilly <ozreilly@163.com>
2018-11-12 16:26:55 +0800

    ejs 测试

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var app = require('koa')()
-  , logger = require('koa-logger')
-  , json = require('koa-json')
-  , views = require('koa-views')
-  , onerror = require('koa-onerror');
-
+var app = require('koa')(),
+  logger = require('koa-logger'),
+  json = require('koa-json'),
+  views = require('koa-views'),
+  onerror = require('koa-onerror');
+const ejs = require('ejs');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -13,13 +13,15 @@ onerror(app);
 // global middlewares
 app.use(views('views', {
   root: __dirname + '/views',
-  default: 'jade'
+  map: {
+    html: 'ejs'
+  }
 }));
 app.use(require('koa-bodyparser')());
 app.use(json());
 app.use(logger());
 
-app.use(function *(next){
+app.use(function* (next) {
   var start = new Date;
   yield next;
   var ms = new Date - start;
